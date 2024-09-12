b6d044e1a5172bb3d43311ddac3ade7b16167d52
Janne Adolf <janne.adolf@inbo.be>
2024-09-12 10:19:06 +0200

    update script for project setup

--- source/setup/setup_renv_checklist.R
+++ source/setup/setup_renv_checklist.R
@@ -1,16 +1,24 @@
 # initialize renv
 renv::init()
-
-# install checklist
+#
+# Enable the INBO universe
 options(
   repos = c(
     inbo = "https://inbo.r-universe.dev", CRAN = "https://cloud.r-project.org"
   )
 )
+#
+# install checklist
 install.packages("checklist")
-
+#
+# set-up project
+checklist::setup_project("path to project folder here")
+#
+# install INBOtheme
+install.packages("INBOtheme")
+#
+# install INBOmd
+install.packages("INBOmd")
+#
 # write renv lockfile
 renv::snapshot()
-
-# set-up project
-checklist::setup_project("C:/Users/janne_adolf/github/test-poweranalyse")
