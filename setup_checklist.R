41e76396da98ac82d0b40f636ddbb80034ced788
Janne Adolf <janne.adolf@inbo.be>
2024-09-12 09:42:31 +0200

    add script to set up checklist tools

---
+++ setup_checklist.R
@@ -0,0 +1,10 @@
+# install checklist
+options(
+  repos = c(
+    inbo = "https://inbo.r-universe.dev", CRAN = "https://cloud.r-project.org"
+  )
+)
+install.packages("checklist")
+
+# set-up project
+checklist::setup_project("C:/Users/janne_adolf/github/test-poweranalyse")
