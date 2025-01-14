9ef450fcd0d34c4cc7df2a650fcfe00d017c2a3a
Janne Adolf <janne.adolf@inbo.be>
2025-01-14 10:09:34 +0100

    add information on using inbomd

---
+++ source/setup/use_inbomd.R
@@ -0,0 +1,13 @@
+# render presentation via build pane 
+# in case build pane is not available: activate it via:
+# Build > Configure Build Tools > 
+# Project build tools: Website & Site directory: directory of index.Rmd
+
+# INBOmd introduction
+# https://inbo.github.io/INBOmd/articles/introduction.html
+
+# example source 
+# https://github.com/inbo/inbomd_examples/blob/main/source/flanders_slides/index.Rmd
+# example rendered
+# https://inbo.github.io/inbomd_examples/flanders_slides/flanders_slides.pdf
+
